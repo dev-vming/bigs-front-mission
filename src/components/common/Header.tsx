af893569cc4ecab62a92a88af86e0cffdf06cd91
dev-vming <dev.vming@gmail.com>
2026-01-25 23:54:31 +0900

    add: 로그아웃, 뒤로가기 시 확인 모달 적용

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -2,23 +2,43 @@
 
 import { FaAngleLeft } from "react-icons/fa";
 import UserMenu from "./UserMenu";
+import { useModalStore } from "@/stores/modalStore";
+import { useRouter } from "next/navigation";
 
 interface HeaderProps {
     title: string;
 }
 
 export default function Header({ title }: HeaderProps) {
+    // 라우터
+    const router = useRouter();
     // title이 게시판인지 확인
     const isBoardList = title === "게시판";
     // title이 수정, 등록 상태인지 확인
     const isEditing = title === "게시물 수정" || title === "게시물 등록";
+    // 모달 상태 수정
+    const setModal = useModalStore((state) => state.setModal);
+    // 뒤로 가기 로직 처리
+    const handleBack = () => {
+        router.back();
+    };
 
     return (
         <header className="w-full border-b border-gray-200 bg-white">
             <div className="relative h-14 mx-auto px-4 flex items-center">
                 {/* 왼쪽 영역 (모바일 전용 뒤로가기) */}
                 {!isBoardList && (
-                    <button className="p-2 md:hidden">
+                    <button
+                        className="p-2 md:hidden cursor-pointer"
+                        onClick={() =>
+                            setModal({
+                                open: true,
+                                title: "알림",
+                                content: '작성을 취소하시겠습니까? \n 작성 중인 내용이 저장되지 않습니다.',
+                                onConfirm: handleBack,
+                            })
+                        }
+                    >
                         <FaAngleLeft className="w-5 h-5 text-gray-500" />
                     </button>
                 )}
@@ -43,7 +63,7 @@ export default function Header({ title }: HeaderProps) {
                             <UserMenu name="개발자" email="email@test.com" />
                         </div>
                     )}
-                    
+
                     {/* 모바일: 작성/수정일 때만 완료 버튼 */}
                     {isEditing && (
                         <button className="p-2 md:hidden text-sm font-semibold text-blue-600">
--- src/components/common/UserMenu.tsx
+++ src/components/common/UserMenu.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useModalStore } from "@/stores/modalStore";
 import { useEffect, useRef, useState } from "react";
 import { FaUser } from "react-icons/fa";
 
@@ -9,10 +10,17 @@ interface UserMenuProps {
 }
 
 export default function UserMenu({ name, email }: UserMenuProps) {
+    // 모달 상태 수정
+    const setModal = useModalStore((state) => state.setModal);
     // 팝업 열림 닫힘 상태
     const [open, setOpen] = useState(false);
     // 컴포넌트 영역 ref
     const containerRef = useRef<HTMLDivElement>(null);
+
+    // 로그아웃 로직 처리
+    const handleLogout = () => {
+        console.log("로그아웃");
+    };
 
     // 외부 클릭 시 닫힘 처리
     useEffect(() => {
@@ -33,11 +41,6 @@ export default function UserMenu({ name, email }: UserMenuProps) {
         };
     }, [open]);
 
-    // 로그아웃 로직 처리
-    const onLogout = () => {
-        console.log("로그아웃");
-    };
-
     return (
         <div ref={containerRef} className="relative">
             {/* 버튼 영역 */}
@@ -56,12 +59,17 @@ export default function UserMenu({ name, email }: UserMenuProps) {
                             <p className="text-sm font-medium text-gray-900">
                                 {name}
                             </p>
-                            <p className="text-xs text-gray-500">
-                                {email}
-                            </p>
+                            <p className="text-xs text-gray-500">{email}</p>
                         </div>
                         <button
-                            onClick={onLogout}
+                            onClick={() =>
+                                setModal({
+                                    open: true,
+                                    title: "알림",
+                                    content: "로그아웃 하시겠습니까?",
+                                    onConfirm: handleLogout,
+                                })
+                            }
                             className="w-full px-4 py-2 text-sm text-center text-red-600 hover:bg-gray-50 cursor-pointer"
                         >
                             로그아웃
