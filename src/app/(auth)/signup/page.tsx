47b1c2bb82303a9eb11625999b7b8a93885f0f73
dev-vming <dev.vming@gmail.com>
2026-01-25 21:39:53 +0900

    add: 로그인 회원가입 (auth) ui 생성

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,11 +1,14 @@
+import SignupForm from "@/components/auth/SignupForm";
+
 export const metadata = {
     title: "회원가입",
 };
 
 export default function SignupPage() {
     return (
-        <div>
-            회원가입 페이지
+        <div className="w-full max-w-sm">
+            <h1 className="text-2xl font-bold mb-6 text-center">회원가입</h1>
+            <SignupForm />
         </div>
     );
 }
