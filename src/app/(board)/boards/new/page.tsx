8982880aee2c80ab2dabc21b1c8379a0c60dc73f
dev-vming <dev.vming@gmail.com>
2026-01-27 03:19:17 +0900

    add: 글 작성 api 연동

--- src/app/(board)/boards/new/page.tsx
+++ src/app/(board)/boards/new/page.tsx
@@ -1,10 +1,39 @@
+"use client";
+
+import { useMutation } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
 import BoardForm from "@/components/boards/BoardForm";
+import { boardsApi } from "@/lib/api/boards";
+import { BoardFormValues } from "@/schemas/boardScema";
 
 export default function BoardNewPage() {
+    // 라우터
+    const router = useRouter();
+
+    // 글작성 뮤테이션
+    const createBoardMutation = useMutation({
+        mutationFn: boardsApi.createBoard,
+        onSuccess: (data) => {
+            router.replace(`/boards/${data.id}`);
+        },
+    });
+
+    const handleCreateBoard = (values: BoardFormValues) => {
+        createBoardMutation.mutate({
+            request: {
+                title: values.title,
+                content: values.content,
+                category: values.category,
+            },
+            file: values.file,
+        });
+    };
     return (
         <div>
-            <BoardForm />
+            <BoardForm
+                onSubmit={handleCreateBoard}
+                isPending={createBoardMutation.isPending}
+            />
         </div>
     );
 }
-
