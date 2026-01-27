21bd0ed41157b37acb5e018cceae987bf4924699
dev-vming <dev.vming@gmail.com>
2026-01-28 05:46:01 +0900

    fix: nextconfig images.domains 방식에서 images.remotePatterns로 변경

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
-    domains: ['front-mission.bigs.or.kr'],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "front-mission.bigs.or.kr",
+        pathname: "/media/images/**",
+      },
+    ],
   },
 };
 
